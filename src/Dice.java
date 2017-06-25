3f1f6ce040f4f279d0bc8e806b52090d3d03d260
Daniel <danielsaka@hotmail.com>
2017-06-25 22:14:07 +0200

    eagleLord commits stuff

--- src/Dice.java
+++ src/Dice.java
@@ -1,8 +1,31 @@
+import java.util.Random;
 
 public class Dice {
-	private int dice;
+	private int currentValue;
+	private final int sides;
+	private Random random = new Random();
 	
-	public i roll(){
+	public Dice(){
+		this.sides = 6;
+		roll();
+	}
+	
+	public Dice(int sides){
+		this.sides = sides;
+		roll();
+	}
+	
+	public int getSides() {
+		return sides;
+	}
+	
+	public int getValue() {
+		return currentValue;
+	}
+
+	public int roll() {
+		currentValue = random.nextInt(sides) + 1;
 		
+		return getValue();
 	}
 }
